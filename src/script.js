e4cf30822fd8d515a8c04d31cc520e74294583cc
Lucas Pires Esteves Costa <lucasestevescosta@gmail.com>
2025-04-01 17:56:00 -0300

    add updatetask and updateTaskContet functions

--- src/script.js
+++ src/script.js
@@ -15,7 +15,7 @@ function handleTaskInput() {
 
         saveTask(taskText);
         updateUI();
-    } catch(error) {
+    } catch (error) {
         console.error('Error handling user input: ', error);
         return false
     };
@@ -89,7 +89,7 @@ function saveTask(taskText) {
 };
 
 /**
- * Creates an html task element with a remove button. Uses bootstrap classes for style. Receives the text object with all properties and puts the removeTask() function inside the button.
+ * Creates an html task element with a remove and update button, also transforms the task name into a button to show task details. Uses bootstrap classes for style. Receives the text object with all properties and puts the removeTask(), updateTask(), showDetails() functions and a checkbox for status change inside the button.
  * @param {object} task
  * @returns {object} - html element
  */
@@ -105,6 +105,13 @@ function createTaskElement(task) {
 
         // task text
         const taskContent = document.createElement('span');
+        // taskContent.classList.add('showDetailsButton')
+        // taskContent.onclick = function () {
+        //     showTaskDetails(task.id);
+        // };
+        taskContent.onclick = function () {
+            updateTaskContent(task.id, taskContent);
+        };
         taskContent.textContent = task.text;
 
         // task checkbox
@@ -127,23 +134,22 @@ function createTaskElement(task) {
             removeTask(task.id);
         };
 
-        // update button
-        const updateButton = document.createElement('button');
-        updateButton.type = 'button';
-        // icon
-        const updateIcon = document.createElement('i');
-        updateIcon.classList.add('fa-solid', 'fa-pencil');
-        updateButton.appendChild(updateIcon);
-        
-        updateButton.classList.add('btn', 'btn-secondary', 'me-1', 'ms-1');
-        updateButton.onclick = function () {
-            updatedTask(task.id);
-        };
+        // // update button
+        // const updateButton = document.createElement('button');
+        // updateButton.type = 'button';
+        // // icon
+        // const updateIcon = document.createElement('i');
+        // updateIcon.classList.add('fa-solid', 'fa-pencil');
+        // updateButton.appendChild(updateIcon);
+        // updateButton.classList.add('btn', 'btn-secondary', 'me-1', 'ms-1');
+        // updateButton.onclick = function () {
+        //     updatedTask(task.id);
+        // };
 
         // buttons div
         const buttonsDiv = document.createElement('div');
         buttonsDiv.classList.add('d-flex');
-        buttonsDiv.appendChild(updateButton);
+        // buttonsDiv.appendChild(updateButton);
         buttonsDiv.appendChild(removeButton);
 
         // adding the components to the main task item
@@ -151,13 +157,14 @@ function createTaskElement(task) {
         taskContentDiv.appendChild(taskContent)
         taskElement.appendChild(taskContentDiv);
         taskElement.appendChild(buttonsDiv);
-        
+
         return taskElement;
     } catch (error) {
         console.error('Error creating task element: ', error);
         return null
     }
 };
+
 
 /**
  * Uses getTasks() to retrieve all tasks and then resets the UI. Uses createTaskElement() to creat an HTML task object and append it in taskList.
@@ -182,7 +189,8 @@ function updateUI() {
 
 /**
  * Gets the id from the task to remove from the createTaskElement() whe the task HTML element is created. USes getTasks() to retrieve all tasks, finds the index of the desired task and removes it using splice. Then it calls updateUI().
- * @param {*} idToRemove 
+ * @param {*} idToRemove - Id given on the HTML element creation to find the desired task to remove
+ * @returns {boolean} - returns true if the operation worked or false if it fails
  */
 function removeTask(idToRemove) {
     try {
@@ -192,17 +200,73 @@ function removeTask(idToRemove) {
         storeTasks(taskList);
         updateUI()
         return true;
-    } catch(error) {
+    } catch (error) {
         console.error('Error removing task: ', error);
         return false;
     };
 };
 
+
 function toggleTaskStatus(taskId) {
-    return;
+    try {
+
+        return true;
+    } catch (error) {
+        console.error('Error toggling the task status: ', error);
+        return false;
+    }
 };
 
 
-function updatedTask(taskId) {
-    return;
+function updateTaskContent(taskId, textContentElement) {
+    try {
+        if (!textContentElement || textContentElement.tagName !== 'SPAN') {
+            return false;
+        };
+        const inputElement = switchSpanToInput(textContentElement)
+        inputElement.addEventListener('focusout', () => {
+            updatedTask(taskId, {text: inputElement.value});
+            updateUI()
+        });
+        return true;
+    } catch (error) {
+        console.error('Error updating the content of the task: ', error);
+        return false;
+    }
+}
+
+
+function switchSpanToInput(textContentElement) { // textContentElement is a span element with the task text
+    // input element
+    const inputElement = document.createElement('input');
+    inputElement.type = 'text';
+    inputElement.value = textContentElement.textContent; // Copy text from span element to input element
+
+    // replace span element with input element
+    textContentElement.replaceWith(inputElement);
+
+    return inputElement
+};
+
+/**
+ * Gets the id from the task to update from the createTaskElement() whe the task HTML element is created. USes getTasks() to retrieve all tasks, finds the index of the desired task and uses the spread operator to update only the desired filed. Then it calls updateUI().
+ * @param {*} taskId - Id given on the HTML element creation to find the desired task to update
+ * @param {*} update - task properties to update and it's content
+ * @returns {boolean} - true if the operation is completed or false if it fails
+ */
+function updatedTask(taskId, update) {
+    try {
+        const taskList = getTasks();
+        const taskIndex = taskList.findIndex((task) => task.id === taskId);
+
+        if (taskIndex !== -1) {
+            taskList[taskIndex] = { ...taskList[taskIndex], ...update };
+            storeTasks(taskList);
+            return true;
+        }
+        return false; // returns false if the index check on the if statement fails
+    } catch (error) {
+        console.error('Error updating the task: ', error);
+        return false;
+    }
 };
